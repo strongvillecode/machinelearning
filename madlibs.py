6354336b5ae1683884707cca71d19ce2f0d7f88d
strongvillecode <strongvilleengineering@gmail.com>
2022-09-16 13:16:49 +0000

    project1

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+
+# string concatenation (aka how to put strings together)
+# suppose we want to create a string that says "subscribe to ___"
+youtuber = "" #some string variable
+
+# a few ways to do this
+print("subscribe to " + youtuber)
+print("subscribe to {}" .format(youtuber))
+print(f"subscribe to {youtuber}")
+
